d940cf310b7c41df7ffa92a66bd24de60206a703
Matt Beruldsen <mberulds@gmail.com>
2023-11-14 23:09:56 +1100

    added footer, dashboard page, began creating links to each page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,31 @@
 class PagesController < ApplicationController
+  before_action :set_user, :set_event
   skip_before_action :authenticate_user!, only: [ :home, :search_events ]
 
   def home
-    @users = User.all
-    @events = Event.all
+
   end
 
   def search_events
-    @users = User.all
-    @events = Event.all
     query = params[:query]
     @api_events = ApiService.call_google_events_api(query)
     render :home
   rescue StandardError => e
     @error_message = "Error occurred: #{e.message}"
     render :home
+  end
+
+  def dashboard
+
+  end
+
+  private
+
+  def set_user
+    @users = User.all
+  end
+
+  def set_event
+    @events = Event.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
-  
+
   root to: "pages#home"
-  
-  get '/search_events', to: 'pages#search_events', as: 'search_events' 
+
+  get '/search_events', to: 'pages#search_events', as: 'search_events'
   get 'itinerary_events/new'
   get 'bookmarks/new'
-  
+  get 'pages/dashboard'
+
   resources :events, only: [:new, :show, :create, :destroy, :edit, :update, :index]
   resources :itineraries, only: [:new, :create, :show, :destroy, :edit, :update]
 end

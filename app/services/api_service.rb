8d0f01c1800f8271f230b029f6d401f69b6d0827
aresnelis <129151965+aresnelis@users.noreply.github.com>
2023-11-14 21:30:05 +1100

    Update app/services/api_service.rb
    Co-authored-by: Matt Beruldsen <134824154+berulds@users.noreply.github.com>

--- app/services/api_service.rb
+++ app/services/api_service.rb
@@ -3,7 +3,6 @@ class ApiService
   require "open-uri"
 
   def self.call_google_events_api(query)
-    apikey = "1f1ca290a118669a99ce71eaf91670d49d74de9b10e59974777bd8d7355b3909"
     url = "https://serpapi.com/search.json?engine=google_events&q=#{URI.encode_www_form_component(query)}&hl=en&gl=us&api_key=#{apikey}&start="
 
     events_serialized = URI.open(url).read

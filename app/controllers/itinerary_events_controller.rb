68452212d55bd86d43d154e0731cd84a92e12f69
Matt Beruldsen <mberulds@gmail.com>
2023-11-17 13:35:58 +1100

    created some basic event cards, updated the itinerary new action, multiple layout updates

--- app/controllers/itinerary_events_controller.rb
+++ app/controllers/itinerary_events_controller.rb
@@ -1,6 +1,18 @@
 class ItineraryEventsController < ApplicationController
+
   def new
     @itinerary_event = ItineraryEvent.new
+    @itinerary = Itinerary.find(params[:itinerary_id])
+    @event = Event.find(params[:event_id])
     # authorize @itinerary_event # commented out till we decide if we pundit or not
   end
+
+  def create
+    @itinerary = Itinerary.find(params[:itinerary_id])
+    @event = Event.find(params[:event_id])
+    @itinerary_event = ItineraryEvent.new(itinerary: @itinerary, event: @event)
+    @itinerary_event.save
+    redirect_to pages_dashboard_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   get '/search_events', to: 'pages#search_events', as: 'search_events'
-  get 'itinerary_events/new'
   get 'bookmarks/new'
   get 'pages/dashboard'
   get 'pages/about_us'
 
   resources :events, only: [:new, :show, :create, :destroy, :edit, :update, :index]
-  resources :itineraries, only: [:new, :create, :show, :destroy, :edit, :update]
+  resources :itineraries do
+    resources :itinerary_events, only: [:new, :create]
+  end
 end

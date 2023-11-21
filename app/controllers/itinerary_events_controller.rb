364c9d823f387fa9d6e7fea43fa24212cfa0a91c
Matt Beruldsen <mberulds@gmail.com>
2023-11-21 21:47:49 +1100

    fixed event page layout, fixed route path for adding/crweating bookmark from api search, fixed modal to select event and itinerary ids

--- app/controllers/itinerary_events_controller.rb
+++ app/controllers/itinerary_events_controller.rb
@@ -1,8 +1,16 @@
 class ItineraryEventsController < ApplicationController
 
   def create
+    if params[:query].present?
+      @event = Event.find(params[:query])
+      respond_to do |format|
+        format.json { render json: @event } # Render JSON response
+        format.html # Render HTML template
+        end
+    else
+      @event = Event.find(params[:event_id])
+    end
     @itinerary = Itinerary.find(params[:itinerary_id])
-    @event = Event.find(params[:event_id])
     @itinerary_event = ItineraryEvent.new(itinerary_id: @itinerary.id, event_id: @event.id)
     @itinerary_event.save
     redirect_to itinerary_path(@itinerary)

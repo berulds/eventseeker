519288b371333b1885e5aa3eb6bc84f0d851a910
etienne frechet <efrechet@hotmail.fr>
2023-12-02 11:27:56 +0800

    added modal logic for event save in home page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,7 +10,6 @@ class EventsController < ApplicationController
 
   def new
     @event = Event.new
-    # authorize @event
   end
 
   def create
@@ -33,52 +32,53 @@ class EventsController < ApplicationController
 
       @event = Event.new(event_params_api)
       @event.download_image_from_url(params["thumbnail"])
-      raise
+
       if @event.valid?
         if @event.save
           existing_bookmark = Bookmark.find_by(user_id: current_user.id, event_id: @event.id)
-
           unless existing_bookmark
             Bookmark.create(user_id: current_user.id, event_id: @event.id)
           end
-
           chatroom = Chatroom.create(name: "#{@event.name}", event: @event)
-
-          render json: { status: "success", message: "Event saved successfully" }
-        else
-          render json: { status: "error", errors: @event.errors.full_messages }, status: :unprocessable_entity
         end
-      else
         existing_bookmark = Bookmark.find_by(user_id: current_user.id, event_id: @event.id)
-
         unless existing_bookmark
           Bookmark.create(user_id: current_user.id, event_id: @event.id)
         end
-
-        render json: { status: "success", message: "Event already added" }
       end
     end
-# will refactor that later
-
 
   def show
-    # authorize @event
   end
 
   def edit
-    # authorize @event
   end
 
   def update
     @event.update(event_params)
-    # authorize @event
     redirect_to event_path(@event)
   end
 
   def destroy
     @event.destroy
-    # authorize @event
     redirect_to events_path, status: :see_other
+  end
+
+  def check_event_exists
+    name = params[:name]
+
+    begin
+      existing_event = Event.find_by(name: name)
+
+      if existing_event
+        render json: { exists: true }
+      else
+        render json: { exists: false }
+      end
+    rescue => e
+      Rails.logger.error("Error checking event existence: #{e.message}")
+      render json: { exists: false, error: e.message }, status: :internal_server_error
+    end
   end
 
   private
@@ -91,11 +91,7 @@ class EventsController < ApplicationController
     params.require(:event).permit(:name, :address, :start_time, :end_time, :description, :ticket_purchase)
   end
 
-  def search_path_params
-    params.permit(:query, :date)
-  end
-
   def set_event
-    @event = Event.find(params[:id])
+    @event = Event.find(params[:id]) if params[:id].present?
   end
 end
--- app/javascript/controllers/button_logic_controller.js
+++ app/javascript/controllers/button_logic_controller.js
@@ -1,28 +1,14 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["saveButtonForm"];
-
-  connect() {
-    console.log("ButtonLogicController connected");
-  }
+  static targets = ["saveButtonForm", "eventName"];
 
   async handleSaveClick(event) {
-    // event.preventDefault();
-    console.log("Save Event button clicked");
+      const eventName = this.eventNameTarget.textContent;
 
     try {
-      const form = event.currentTarget.form;
-
-      if (!form) {
-        throw new Error('Missing form element for "button-logic" controller');
-      }
-
-      const formData = new FormData(form);
-
-      const response = await fetch(form.action, {
-        method: form.method,
-        body: formData,
+      const response = await fetch(`/events/check_event_exists?name=${encodeURIComponent(eventName)}`, {
+        method: 'GET',
       });
 
       if (!response.ok) {
@@ -31,17 +17,12 @@ export default class extends Controller {
 
       const data = await response.json();
 
-      if (data.status === "success") {
-        console.log(data);
-
-
-        const successModal = new bootstrap.Modal(document.getElementById("successModal"));
-        successModal.show();
-      } else {
-        console.error("Failed to save event", data.message);
-
+      if (data.exists) {
         const errorModal = new bootstrap.Modal(document.getElementById("errorModal"));
         errorModal.show();
+      } else {
+        const successModal = new bootstrap.Modal(document.getElementById("successModal"));
+        successModal.show();
       }
     } catch (error) {
       console.error("Save Event failed", error);

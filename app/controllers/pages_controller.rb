a68d2db04883b20056377eed518f3104c23a8d79
Isabelle Lau <isabellelaususan@gmail.com>
2023-12-06 17:50:57 +0800

    added event list view cards and itinerary list view cards, still more work to do here

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -28,8 +28,7 @@ class PagesController < ApplicationController
   def dashboard
     @bookmarks = current_user.bookmarks.includes(event: :chatroom)
     @itinerary = current_user.itineraries
-    @first_chatroom =  @bookmarks.find { |bookmark| bookmark.event.chatroom.present? && bookmark.user }&.event&.chatroom.id
-    # raise
+    @first_chatroom = @bookmarks.find { |bookmark| bookmark.event&.chatroom.present? && bookmark.user }&.event&.chatroom&.id
     @bookmarks.each do |bookmark|
       update_status_with_time(bookmark)
     end
